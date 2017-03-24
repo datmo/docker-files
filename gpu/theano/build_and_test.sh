b77cdad64159b8d46117fa6c9eab9b5f2df5564d
Ajaya Agrawal <ajku.agr@gmail.com>
2017-03-24 21:08:39 +0000

    theano working with all deps

--- gpu/theano/build_and_test.sh
+++ gpu/theano/build_and_test.sh
@@ -1,4 +1,4 @@
 docker build -t theano_gpu:14.04 .
 docker rm theano_gpu || true
-nvidia-docker run --rm --name theano_gpu theano_gpu:14.04 python /test/theano_test.py
+nvidia-docker run --rm --name theano_gpu -e 'THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32' theano_gpu:14.04 python /test/theano_test.py
 
