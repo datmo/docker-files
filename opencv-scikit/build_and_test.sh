71d2127c4179662ddb73727db6eb82b95f6c702c
Shabaz Patel <shabaz.b.patel@gmail.com>
2017-03-25 19:36:12 -0700

    fixing test

--- opencv-scikit/build_and_test.sh
+++ opencv-scikit/build_and_test.sh
@@ -1,3 +1,3 @@
 docker build -t opencv-scikit:14.04 .
 docker rm opencv-scikit || true
-nvidia-docker run --rm --name opencv-scikit opencv-scikit:14.04 python /test/opencv_scikit_test.py
+docker run --rm --name opencv-scikit opencv-scikit:14.04 python /test/opencv_scikit_test.py
