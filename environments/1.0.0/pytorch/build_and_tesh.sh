cce504283aef55213128738fa2fe8ab6b9ee7c51
Shabaz Patel <shabaz.b.patel@gmail.com>
2018-07-06 17:15:54 -0700

    updating test for pytorch

--- environments/1.0.0/pytorch/build_and_tesh.sh
+++ environments/1.0.0/pytorch/build_and_tesh.sh
@@ -1,8 +1,8 @@
-docker build -f Dockerfile.py2 -t datmo/pytorch:py27-cpu .
-docker build -f Dockerfile.py2.gpu -t datmo/pytorch:py27-gpu .
-docker build -f Dockerfile.py3 -t datmo/pytorch:py35-cpu .
-docker build -f Dockerfile.py3.gpu -t datmo/pytorch:py35-gpu .
-docker run -v $PWD:/tmp/ -t datmo/pytorch:py27-cpu python /tmp/pytorch_test.py
-docker run -v $PWD:/tmp/ -t datmo/pytorch:py27-gpu python /tmp/pytorch_test.py
-docker run -v $PWD:/tmp/ -t datmo/pytorch:py35-cpu python /tmp/pytorch_test.py
-docker run -v $PWD:/tmp/ -t datmo/pytorch:py35-gpu python /tmp/pytorch_test.py
+docker build -f Dockerfile.py2 -t datmo/pytorch:cpu-py27 .
+docker build -f Dockerfile.py2.gpu -t datmo/pytorch:gpu-py27 .
+docker build -f Dockerfile.py3 -t datmo/pytorch:cpu-py35 .
+docker build -f Dockerfile.py3.gpu -t datmo/pytorch:gpu-py35 .
+docker run -v $PWD:/tmp/ -t datmo/pytorch:cpu-py27 python /tmp/pytorch_test.py
+docker run -v $PWD:/tmp/ -t datmo/pytorch:gpu-py27 python /tmp/pytorch_test.py
+docker run -v $PWD:/tmp/ -t datmo/pytorch:cpu-py35 python /tmp/pytorch_test.py
+docker run -v $PWD:/tmp/ -t datmo/pytorch:gpu-py35 python /tmp/pytorch_test.py
